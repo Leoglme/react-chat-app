820da2f56c5dcd35e3502aea49301a15ee831b95
Léo Guillaume <leo.guillaume@gmail.com>
2021-12-27 13:21:36 +0100

    remove build for deploy vercel

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Messages from "./Chat/Messages";
 import HeaderChat from "./Chat/HeaderChat";
+import MessageField from "./Chat/MessageField";
 
 function ChatBox({users}) {
     return (<>
         <HeaderChat users={users} title={"chat"}/>
         <Messages/>
+        <MessageField/>
     </>);
 }
 
